dea983493284813c855123db05d4ec739ef91fd6
Fábio Almeida <fabio-r-almeida@hotmail.com>
2022-10-27 21:46:49 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,22 +7,22 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '0.0.13'
-DESCRIPTION = 'Streaming video data via networks'
-LONG_DESCRIPTION = 'A package that allows to build simple streams of video, audio and camera data.'
+VERSION = '0.0.1'
+DESCRIPTION = 'A library used to simulate photovoltaic energy production using PVGIS'
+LONG_DESCRIPTION = 'A package that allows to build systems and simulate energy production using real-world data from PVGIS.'
 
 # Setting up
 setup(
-    name="vidstream",
+    name="pv-sim",
     version=VERSION,
-    author="NeuralNine (Florian Dedov)",
-    author_email="<mail@neuralnine.com>",
+    author="Fábio Almeida",
+    author_email="<fabio-r-almeida@outlook.com>",
     description=DESCRIPTION,
     long_description_content_type="text/markdown",
     long_description=long_description,
     packages=find_packages(),
-    install_requires=['opencv-python', 'pyautogui', 'pyaudio'],
-    keywords=['python', 'video', 'stream', 'video stream', 'camera stream', 'sockets'],
+    install_requires=['requests', 'pandas', 'json', 'tzwhere','geopy','numpy','warnings'],
+    keywords=['python', 'PVGIS', 'simulator', 'photovoltaic', 'solar energy', 'solar panels','solar simulation'],
     classifiers=[
         "Development Status :: 1 - Planning",
         "Intended Audience :: Developers",
