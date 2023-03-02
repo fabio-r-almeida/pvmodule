156f51707b5da500941475332b9956315e5ad810
Fábio Almeida <fabio-r-almeida@hotmail.com>
2023-03-02 11:19:20 +0000

    .

--- pvmodule/inverter.py
+++ pvmodule/inverter.py
@@ -1,7 +1,7 @@
 #@title FINAL (TESE) Inverters class { display-mode: "form" }
 class Inverters():
-    def __init__(self, url:str='https://raw.githubusercontent.com/fabio-r-almeida/pvmodule/main/CEC%20Inverters.csv'):
-      self.url = 'https://raw.githubusercontent.com/fabio-r-almeida/pvmodule/main/CEC%20Inverters.csv'
+    def __init__(self, url:str='https://raw.githubusercontent.com/fabio-r-almeida/pvmodule/main/CEC_Inverters.csv'):
+      self.url = 'https://raw.githubusercontent.com/fabio-r-almeida/pvmodule/main/CEC_Inverters.csv'
 
     def inverter(self,name):
       """
@@ -24,13 +24,13 @@ class Inverters():
       List of inverters provided by CEC.
       Parameters
       ----------
-      url : str, default = 'https://raw.githubusercontent.com/fabio-r-almeida/pvmodule/main/CEC%20Inverters.csv'
+      url : str, default = 'https://raw.githubusercontent.com/fabio-r-almeida/pvmodule/main/CEC_Inverters.csv'
           Url to the list of inverters. Can also be a .csv file.
       vac : str, default = None
         Filters the results that are equal to the AC voltage output
 
       pmax : int, default = None
-        Filters the results that are equal to the Max Power input
+        Filters the results that are equal to the Max Power output
 
       print_list : bool, default = False
         Prints list of inverters
@@ -40,55 +40,47 @@ class Inverters():
       inverters = pd.read_csv(self.url).replace(" ", "")
 
       if vac != None:
-        inverters = inverters.loc[inverters['Vac'] == int(vac)]
+        inverters = inverters.loc[inverters['Nominal Voltage (Vac)'] == int(vac)]
       if pmax != None:
-        inverters = inverters.loc[inverters['Paco'] == int(pmax)]
+        inverters = inverters.loc[inverters['Maximum Continuous Output Power (kW)'] == int(pmax)]
       from tabulate import tabulate
       if print_list:
         print(tabulate(inverters, headers='keys', tablefmt='psql'))
 
       return inverters
 
+
+
     def auto_select_inverter(self,module):
       import pandas as pd
 
       number_of_modules = module['modules_per_string'] * module['number_of_strings']
-      Paco = number_of_modules * module['pdc']
+      Max_Input_DC_Power = number_of_modules * module['pdc'] / 1000
       Vdcmax = module['modules_per_string'] * module['uoc']
       Idcmax =  module['number_of_strings'] * module['isc']
 
-
       inverter_list = Inverters().list_inverters(print_list=False)
-      inverter_list = inverter_list.loc[inverter_list['Paco'] >= Paco]
-      inverter_list = inverter_list.loc[inverter_list['Paco'] <= Paco*2.5]
-      inverter_list = inverter_list.loc[inverter_list['Vdcmax'] >= Vdcmax]
-      inverter_list = inverter_list.loc[inverter_list['Mppt_high'] >= Vdcmax]
-      inverter_list = inverter_list.loc[inverter_list['Mppt_low'] <= Vdcmax]
-      inverter_list = inverter_list.loc[inverter_list['Idcmax'] >= Idcmax]
+
+
+      
+
+      inverter_list = inverter_list.loc[inverter_list['Maximum Continuous Output Power (kW)'] >= Max_Input_DC_Power]
+      inverter_list = inverter_list.loc[inverter_list['Voltage Maximum (Vdc)'] >= Vdcmax] #MAX MPPT
+      inverter_list = inverter_list.loc[inverter_list['Voltage Minimum (Vdc)'] <= Vdcmax] #MIN MPPT
+      #inverter_list = inverter_list.loc[inverter_list['Idcmax'] >= Idcmax]   
 
       inverter = pd.DataFrame(inverter_list)
+      inverter['efficiency'] = inverter['Weighted Efficiency (%)']
 
-
-      Pdc_max = inverter['Paco']
-      Pdco = inverter['Pdco']
-      Vdco = inverter['Vdco']
-      C0 = inverter['C0']
-      C1 = inverter['C1']
-      C2 = inverter['C2']
-      C3 = inverter['C3']
-      Pso = inverter['Pso']
-
-      A = Pdco * (1 + C1 * (Vdcmax - Vdco))
-      B = Pso * (1 + C2 * (Vdcmax - Vdco))
-      C = C0 * (1 + C3 * (Vdcmax - Vdco))
-
-      inverter['efficiency'] = (Pdc_max / (A - B) - C * (A - B)) * (Paco - B) + C * (Paco - B)**2
+     
+      inverter = inverter.sort_values(by='Maximum Continuous Output Power (kW)', ascending=True)
 
       if len(inverter) > 0:
-        index = inverter['efficiency'].idxmax()
-        inverter = inverter.drop(['efficiency'], axis=1)
-        return  inverter.loc[[index]]
+        inverter = inverter.sort_values(by='Maximum Continuous Output Power (kW)', ascending=True)
+        inverter = inverter.drop( inverter.index.to_list()[1:] ,axis = 0 )
+        return  inverter
 
       return pd.DataFrame()
+
 
 
