c6bea98bb37de470aab98b1bea714fe515cc8cc1
unknown <fabio-r-almeida@hotmail.com>
2023-04-21 16:15:02 +0100

    Automatic Push

--- upload.bat
+++ upload.bat
@@ -16,9 +16,20 @@ set /p COUNTER=<1_current_version.txt
         >>"%textFile%" echo(!line:%search%=%replace%!
         endlocal
     )
+@echo off
 
+    set "textFile=1_VERSION.txt"
+
+    for /f "delims=" %%i in ('type "%textFile%" ^& break ^> "%textFile%" ') do (
+        set "line=%%i"
+        setlocal enabledelayedexpansion
+        >>"%textFile%" echo(!line:%search%=%replace%!
+        endlocal
+    )
 
 <NUL set /p=%COUNTER%> 1_current_version.txt
+copy 1_VERSION.txt pvmodule_version.py
+timeout 2
 copy 1_SETUP_TEMPLATE.txt setup.py
 timeout 2
 
@@ -31,4 +42,10 @@ timeout 2
 python setup.py sdist bdist_wheel
 twine upload dist/* -u Fabio_R_Almeida
 timeout 2
+
+git add -A
+timeout 2
+git commit -m "Automatic Push"
+timeout 2
+git push -u origin AutoUpdate
 
